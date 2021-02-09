aee7e7e0ac930220c4521d4e9e34ac7f9f18c275
Cesar Hutz <50091474+CCHUTZ@users.noreply.github.com>
2021-02-09 17:32:04 -0600

    Create Salamalenco.java
    Aleikum salam

---
+++ Salamalenco.java
@@ -0,0 +1,16 @@
+public class TenHellos {
+    public static void main (String[] args ) {
+
+        //Primeros casos que no empiezan con Hola
+        System.out.println("1° Hola");
+        System.out.println("2° Hola");
+        System.out.println("3° Hola");
+        // cuenta de i = 4 a 10 
+        int i = 4;
+        while ( i <= 10) {
+            System.out.println(i + "° Salamalenco");
+            i = i + 1;
+    
+        }
+    }
+}
